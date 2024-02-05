7e18eb82e724609aa90dea211a10b4c20b3f9fc6
UcheNwuchu <willnwuchu@gmail.com>
2024-02-05 06:08:55 -0800

    Inheritance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass#!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass#!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass#!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
