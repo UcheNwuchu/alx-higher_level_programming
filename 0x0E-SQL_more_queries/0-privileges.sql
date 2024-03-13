4935b8b6a7649f90a826ddf9f71b4875e30188b0
UcheNwuchu <willnwuchu@gmail.com>
2024-03-13 14:43:42 -0700

    102-rating_shows

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,10 @@
--- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+# MySQL credentials
+MYSQL_USER="root"
+MYSQL_PASSWORD="your_mysql_password"
+
+# MySQL query to list privileges of specified users
+MYSQL_QUERY="SELECT * FROM mysql.user WHERE user IN ('user_0d_1', 'user_0d_2');"
+
+# Execute the query and display results
+mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "$MYSQL_QUERY"
+
